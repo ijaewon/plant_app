c3674778a19bcdacdc6b6c9d07153f47cc5a3c4c
ijaewon <ijaewon480@gmail.com>
2021-03-08 00:42:39 +0900

    Recomends plants card done

--- lib/screen/home/components/body.dart
+++ lib/screen/home/components/body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/constants.dart';
+import 'package:plant_app/screen/home/components/recomends_plants.dart';
 import 'package:plant_app/screen/home/components/title_with_more_btn.dart';
 import 'header_with_search_box.dart';
 
@@ -13,7 +15,46 @@ class Body extends StatelessWidget {
           TitleWithMoreBtn(
             title: "Recomended",
             press: () {},
-          )
+          ),
+          RecomendsPlants()
+        ],
+      ),
+    );
+  }
+}
+
+class RecomendsPlants extends StatelessWidget {
+  const RecomendsPlants({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          RecomendPlanCard(
+            image: "assets/images/image_1.png",
+            title: "Samantha",
+            country: "Russia",
+            price: 440,
+            press: () {},
+          ),
+          RecomendPlanCard(
+            image: "assets/images/image_2.png",
+            title: "Angelica",
+            country: "Russia",
+            price: 440,
+            press: () {},
+          ),
+          RecomendPlanCard(
+            image: "assets/images/image_3.png",
+            title: "Samantha",
+            country: "Russia",
+            price: 440,
+            press: () {},
+          ),
         ],
       ),
     );
